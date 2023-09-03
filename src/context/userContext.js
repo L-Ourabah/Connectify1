fe7d8db84431f5411c9f2e5ae9e4ea70d2841b0e
ourabah <ourabah.lou@hotmail.com>
2023-09-03 23:36:08 +0200

    Create userContext.js

---
+++ src/context/userContext.js
@@ -0,0 +1,38 @@
+import { createContext, useState, useEffect } from 'react';
+
+import{
+  signInWithEmailAndPassword,
+  createUserWithEmailAndPassword,
+  onAuthStateChanged
+} from 'firebase/auth'
+import {auth} from "../services/firebase-config";
+
+export const UserContext = createContext();
+
+export function UserContextProvider(props){
+
+  const signUp = (email, pwd) => createUserWithEmailAndPassword(auth, email, pwd);
+
+  const signIn = (email, pwd) => signInWithEmailAndPassword(auth, email, pwd);
+
+  const [currentUser, setCurrentUser] = useState();
+  const [loadingData, setLoadingData] = useState(true);
+
+  useEffect(() => {
+
+    const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
+      setCurrentUser(currentUser)
+      setLoadingData(false)
+    })
+
+    return unsubscribe;
+
+  }, [])
+
+  return (
+    <UserContext.Provider value={{signUp, signIn, currentUser}}>
+      {!loadingData && props.children}
+    </UserContext.Provider>
+  )
+
+}
