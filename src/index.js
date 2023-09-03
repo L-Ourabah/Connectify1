09935a7383f78e9fd3829cacb94a34084a4b3653
ourabah <ourabah.lou@hotmail.com>
2023-09-03 23:36:10 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,14 +1,53 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
+
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { UserContextProvider } from './context/userContext';
+
+
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import ErrorPage from './Routes/ErrorPage';
+import Connexion from './components/pages/Connexion/Connexion';
+import Inscription from './components/pages/Inscription/Inscription';
+import Profil from './components/pages/Profil/Profil';
+import Contact from './components/pages/Contact/Contact';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    errorElement: <ErrorPage />
+  },
+  {
+    path: "connexion",
+    element: <Connexion />
+  },
+  {
+    path: "inscription",
+    element: <Inscription />
+  },
+  {
+    path: "profil",
+    element: <Profil />
+  },
+  {
+    path: "contact",
+    element: <Contact />
+  }
+])
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <UserContextProvider>
+
+      <RouterProvider router={router} />
+
+    </UserContextProvider>
   </React.StrictMode>
+
+
 );
 
 // If you want to start measuring performance in your app, pass a function
