7322f6d284672664b480e89593cf955c44181170
ourabah <ourabah.lou@hotmail.com>
2023-09-03 23:36:50 +0200

    Create Inscription.js

---
+++ src/components/pages/Inscription/Inscription.js
@@ -0,0 +1,21 @@
+import { UserContextProvider } from '../../../context/userContext';
+import Header from './components/Header';
+import Main from './components/Main';
+import Footer from './components/Footer'
+import React from 'react';
+
+export default function Inscription() {
+    return (
+ <>
+  <UserContextProvider>
+     <Header/>
+     <Main/>
+     <Footer/>
+     </UserContextProvider>
+     </>
+    
+    );
+
+}
+
+ 
