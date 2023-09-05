50ea96e27260db0d6a18de861028abe97f829b04
ourabah <ourabah.lou@hotmail.com>
2023-09-05 16:16:48 +0200

    Update Header.js

---
+++ src/components/pages/Contact/components/Header.js
@@ -0,0 +1,42 @@
+import React, { useState } from 'react';
+import '../../../../App.css';
+import '../../../../appResponsive.css';
+
+import { Link } from 'react-router-dom';
+function openNav() {
+  console.log("open navbar");
+  document.getElementById("myNav").style.width = "100vw";
+}
+
+function closeNav() {
+  console.log("close navbar");
+  document.getElementById("myNav").style.width = "0%";
+}
+
+export default function Header() {
+  return (
+    <header className="header_connexion">
+      <nav className="navbar_deux">
+        <h1 >Connectify</h1>
+        <div className="onglets">   <Link to={'/profil'}>Profil</Link>
+          <Link to={'../contact'}>Contact</Link>
+          <Link className="btn_deconnexion" to={'../connexion'}>Déconnexion</Link></div>
+
+        {/*menu mobile*/}
+        <span className='burgerMenu' onClick={openNav}>☰ </span>
+        <div id="myNav" className="overlay">
+        {/* Utilisation d'un bouton pour fermer le menu mobile */}
+        <button className="closebtn" onClick={closeNav}>×</button>
+          <div className="overlay-content">
+            <h1 >Connectify</h1>
+            <Link to={'/profil'}>Profil</Link>
+            <Link to={'../contact'}>Contact</Link>
+            <Link to={'../connexion'}>Connexion</Link>
+          </div>
+
+        </div>
+
+      </nav>
+    </header>
+  );
+}
