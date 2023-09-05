f3e8ee7d082c7a4c607a04206041cb62a78576ff
ourabah <ourabah.lou@hotmail.com>
2023-09-05 16:17:27 +0200

    Create Connexion.js

---
+++ src/Routes/Connexion.js
@@ -0,0 +1,19 @@
+
+import Header from '../components/pages/Connexion/components/Header';
+import Main from '../components/pages/Connexion/components/Main';
+import Footer from '../components/Commun/Footer';
+import React from 'react';
+
+export default function Connexion() {
+    return (
+       
+        <>
+     <Header/>
+     <Main/>
+     <Footer/>
+     </>
+    );
+
+}
+
+ 
