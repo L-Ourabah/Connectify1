39ec4d967cfc90614d5849460d25a7ea1d20fa08
ourabah <ourabah.lou@hotmail.com>
2023-09-05 16:16:39 +0200

    Update Header.js

--- src/components/pages/Profil/components/Header.js
+++ src/components/pages/Profil/components/Header.js
@@ -1,15 +1,17 @@
-import React from 'react';
+import React, { useState } from 'react';
 import '../../../../App.css';
-import '../../../../appMobile.css'
+import '../../../../appResponsive.css';
 import { Link } from 'react-router-dom';
 
 
 
 function openNav() {
-  document.getElementById("myNav").style.width = "100%";
+  console.log("open navbar");
+  document.getElementById("myNav").style.width = "100vw";
 }
 
 function closeNav() {
+  console.log("close navbar");
   document.getElementById("myNav").style.width = "0%";
 }
 
@@ -21,13 +23,14 @@ export default function Header() {
         <div className="onglets">
           <Link to={'/profil'}>Profil</Link>
           <Link to={'../contact'}>Contact</Link>
-          <Link to={'../connexion'}>Déconnexion</Link>
+          <Link className="btn_deconnexion" to={'../connexion'}>Déconnexion</Link>
         </div>
 
         {/*menu mobile*/}
         <span className='burgerMenu' onClick={openNav}>☰ </span>
         <div id="myNav" className="overlay">
-          <a href="javascript:void(0)" className="closebtn" onClick={closeNav}>×</a>
+         {/* Utilisation d'un bouton pour fermer le menu mobile */}
+        <button className="closebtn" onClick={closeNav}>×</button>
           <div className="overlay-content">
             <h1 >Connectify</h1>
             <Link to={'/profil'}>Profil</Link>
