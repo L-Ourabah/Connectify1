ab8d7d972025f060de7fe273b9b6dff161912e09
ourabah <ourabah.lou@hotmail.com>
2023-09-05 16:17:25 +0200

    Create Contact.js

---
+++ src/Routes/Contact.js
@@ -0,0 +1,16 @@
+import Header from '../components/pages/Contact/components/Header';
+import Main from '../components/pages/Contact/components/Main';
+import Footer from '../components/Commun/Footer';
+import React from 'react';
+
+export default function Contact() {
+    return (
+       
+        <>
+     <Header/>
+     <Main/>
+     <Footer/>
+     </>
+    );
+
+}
