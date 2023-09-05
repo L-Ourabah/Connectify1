2391bfbeddd1d564eb830f660468f1de47432b84
ourabah <ourabah.lou@hotmail.com>
2023-09-05 16:17:33 +0200

    Create Inscription.js

---
+++ src/Routes/Inscription.js
@@ -0,0 +1,20 @@
+
+import Header from '../components/pages/Inscription/components/Header';
+import Main from '../components/pages/Inscription/components/Main';
+import Footer from '../components/pages/Inscription/components/Footer'
+import React from 'react';
+
+export default function Inscription() {
+    return (
+        <>
+            
+                <Header />
+                <Main />
+                <Footer />
+           
+        </>
+
+    );
+
+}
+
