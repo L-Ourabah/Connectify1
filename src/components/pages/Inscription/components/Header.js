c944c95a7789d4c58e807281ff1ba2dc5f209966
ourabah <ourabah.lou@hotmail.com>
2023-09-05 16:17:13 +0200

    Update Header.js

--- src/components/pages/Inscription/components/Header.js
+++ src/components/pages/Inscription/components/Header.js
@@ -1,16 +1,19 @@
-import React from 'react';
+import React, { useState } from 'react';
 import '../../../../App.css';
-import '../../../../appMobile.css'
+import '../../../../appResponsive.css';
 
 import { Link } from 'react-router-dom';
 
 function openNav() {
-  document.getElementById("myNav").style.width = "100%";
+  console.log("open navbar");
+  document.getElementById("myNav").style.width = "100vw";
 }
 
 function closeNav() {
+  console.log("close navbar");
   document.getElementById("myNav").style.width = "0%";
 }
+
 export default function Header() {
   return (
     <header className="header_connexion">
@@ -25,7 +28,8 @@ export default function Header() {
         {/*menu mobile*/}
         <span className='burgerMenu' onClick={openNav}>☰ </span>
         <div id="myNav" className="overlay">
-          <a href="javascript:void(0)" className="closebtn" onClick={closeNav}>×</a>
+        {/* Utilisation d'un bouton pour fermer le menu mobile */}
+        <button className="closebtn" onClick={closeNav}>×</button>
           <div className="overlay-content">
             <h1 >Connectify</h1>
             <Link to={'/'}>Accueil</Link>
