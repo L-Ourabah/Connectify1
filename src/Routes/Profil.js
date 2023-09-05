f75ff77f99a59f4d18e11a1bcb092570ee8e3b1d
ourabah <ourabah.lou@hotmail.com>
2023-09-05 16:17:30 +0200

    Create Profil.js

---
+++ src/Routes/Profil.js
@@ -0,0 +1,18 @@
+
+import Header from '../components/pages/Profil/components/Header';
+import Main from '../components/pages/Profil/components/Main';
+import Footer from '../components/Commun/Footer';
+import React from 'react';
+
+export default function Connexion() {
+    return (
+
+        <>
+            <Header />
+            <Main />
+            <Footer />
+        </>
+    );
+
+}
+
