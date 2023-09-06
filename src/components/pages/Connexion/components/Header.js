2a1fdfef71be635c6027a08c6bf393f8763fe862
ourabah <ourabah.lou@hotmail.com>
2023-09-06 10:51:22 +0200

    Update Header.js

--- src/components/pages/Connexion/components/Header.js
+++ src/components/pages/Connexion/components/Header.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 import '../../../../App.css';
 import '../../../../appResponsive.css';
 import { Link } from 'react-router-dom';
