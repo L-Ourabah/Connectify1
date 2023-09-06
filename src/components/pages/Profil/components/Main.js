1b5fc365053fa6bc9294a94600b1d8c85c48f471
ourabah <ourabah.lou@hotmail.com>
2023-09-06 10:51:33 +0200

    Update Main.js

--- src/components/pages/Profil/components/Main.js
+++ src/components/pages/Profil/components/Main.js
@@ -2,11 +2,29 @@ import React from 'react';
 import '../../../../App.css';
 import '../../../../appResponsive.css';
 
+
+
+
+import SectionVideo from "./sectionVideo";
+
+
+export default function Main() {
+    return(
+        <main>
+    <div className='main_rofil'>
+
+    </div>
+    <SectionVideo/>
+   </main>
+   )
+}
+/*
 export default function Main(){
 
  
     return(
 <main>
+     <sectionVideo/>
     <div className='main_profil'>
     <div className='filtre'>
         <div className='filtre_content'>
@@ -23,12 +41,8 @@ export default function Main(){
         <section className='gallery'>
                 <h2>Gallery</h2>
         </section>
-        <section className='video'>
-                <h2>Video</h2>
-             {/* <video id="video_profil" autoPlay loop muted>
-        <source src={video} type="video/mp4" />
-      </video> */}   
-        </section>
+       
+       
         <section className='music'>
                 <h2>Music</h2>
         </section>
@@ -39,4 +53,8 @@ export default function Main(){
 
     )
 
-}
+}*/
+
+
+
+
