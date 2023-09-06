420c4f712ea96c9a3cd92ce90da205e3f823635e
ourabah <ourabah.lou@hotmail.com>
2023-09-06 10:51:46 +0200

    Update firebase-config.js

--- src/services/firebase-config.js
+++ src/services/firebase-config.js
@@ -1,25 +1,35 @@
 
+import { initializeApp }from "firebase/app";
 import { getAuth } from "firebase/auth";
+import { getFirestore } from "@firebase/firestore";
+import { getStorage } from "firebase/storage"
 
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
+/*const firebaseConfig = {
   apiKey: "AIzaSyAaZ12OTyXEq-qMu--38fc3-kYXUGmWrn0",
   authDomain: "test-3a72e.firebaseapp.com",
   projectId: "test-3a72e",
   storageBucket: "test-3a72e.appspot.com",
   messagingSenderId: "733939924500",
   appId: "1:733939924500:web:f774433164f00d0b3db101",
+  
+};*/
+const firebaseConfig = {
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
   measurementId: "G-Q78H751KKE"
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+export const db = getFirestore(app);
 export const auth = getAuth(app);
+export const storage = getStorage(app);
