73b9cac65a4fb8c3b130d4f0037788cfcfe5a827
ourabah <ourabah.lou@hotmail.com>
2023-09-18 14:49:59 +0200

    Update userContext.js

--- src/context/userContext.js
+++ src/context/userContext.js
@@ -1,38 +1,43 @@
+// Import des dépendances React et Firebase
 import { createContext, useState, useEffect } from 'react';
-
-import{
+import {
   signInWithEmailAndPassword,
   createUserWithEmailAndPassword,
   onAuthStateChanged
 } from 'firebase/auth'
-import {auth} from "../services/firebase-config";
+import { auth } from "../services/firebase-config";
 
+// Création du contexte utilisateur
 export const UserContext = createContext();
 
-export function UserContextProvider(props){
-
+// Composant de contexte utilisateur
+export function UserContextProvider(props) {
+  // Fonction pour s'inscrire avec un email et un mot de passe
   const signUp = (email, pwd) => createUserWithEmailAndPassword(auth, email, pwd);
 
+  // Fonction pour se connecter avec un email et un mot de passe
   const signIn = (email, pwd) => signInWithEmailAndPassword(auth, email, pwd);
 
+  // État pour stocker l'utilisateur actuel
   const [currentUser, setCurrentUser] = useState();
+  // État pour indiquer si les données sont en cours de chargement
   const [loadingData, setLoadingData] = useState(true);
 
+  // Effet qui écoute les changements d'état de l'authentification
   useEffect(() => {
-
     const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
       setCurrentUser(currentUser)
       setLoadingData(false)
     })
 
+    // Désabonnement de l'écoute des changements d'état
     return unsubscribe;
-
   }, [])
 
+  // Rendu du contexte utilisateur et affichage des enfants une fois que les données sont chargées
   return (
-    <UserContext.Provider value={{signUp, signIn, currentUser}}>
+    <UserContext.Provider value={{ signUp, signIn, currentUser }}>
       {!loadingData && props.children}
     </UserContext.Provider>
   )
-
 }
