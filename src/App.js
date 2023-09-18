d23d45d71dc52e17d8f7d642c68eb6e2645119a3
ourabah <ourabah.lou@hotmail.com>
2023-09-18 14:49:46 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 
 import Header from './components/Commun/Header';
-import Main from './components/Commun/Main';
+import Main from './components/pages/Home/components/Main';
 import Footer from './components/Commun/Footer'
 import React from 'react';
 
