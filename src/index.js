166451e68775b201cb15c2eecc5c83f58a293289
ourabah <ourabah.lou@hotmail.com>
2023-09-18 14:48:21 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,8 +1,7 @@
 import React from 'react';
-
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { StrictMode } from 'react';
+import { BrowserRouter, Routes, Route, } from 'react-router-dom';
 import { createRoot } from 'react-dom/client'; // Utilisez createRoot depuis react-dom/client
-
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { UserContextProvider } from './context/userContext';
@@ -12,28 +11,43 @@ import Inscription from './Routes/Inscription';
 import Profil from './Routes/Profil';
 import Contact from './Routes/Contact';
 
+// Sélectionne l'élément HTML avec l'ID "root"
 const root = document.getElementById('root');
 
+// Crée un "appRoot" pour le rendu React
 const appRoot = createRoot(root);
 
+// Rendu de l'application React dans l'élément "root"
 appRoot.render(
-  
+  <StrictMode>
+    {/* Encadre l'application avec React Strict Mode, pour détecter les problèmes potentiels */}
     <UserContextProvider>
+      {/* Fournit un contexte utilisateur aux composants enfants */}
       <BrowserRouter>
+        {/* Configuration du routeur pour gérer la navigation */}
         <Routes>
+          {/* Définition des routes et de leurs composants associés */}
           <Route path="/" element={<App />} />
+          {/* Route pour la page d'accueil */}
           <Route path="connexion" element={<Connexion />} />
+          {/* Route pour la page de connexion */}
           <Route path="inscription" element={<Inscription />} />
+          {/* Route pour la page d'inscription */}
           <Route path="profil" element={<Profil />} />
+          {/* Route pour la page de profil */}
           <Route path="contact" element={<Contact />} />
+          {/* Route pour la page de contact */}
           <Route path="*" element={<ErrorPage />} />
+          {/* Route pour toutes les autres URL non définies */}
         </Routes>
       </BrowserRouter>
     </UserContextProvider>
-  
+  </StrictMode>
 );
 
+// Rapport des performances de l'application
 reportWebVitals();
+
 
 
 
