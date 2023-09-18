6fcfcb9b061209bb43e43dfee88c6d5bb5782e67
ourabah <ourabah.lou@hotmail.com>
2023-09-18 14:50:28 +0200

    Update ErrorPage.js

--- src/Routes/ErrorPage.js
+++ src/Routes/ErrorPage.js
@@ -1,19 +1,20 @@
-import { useRouteError } from "react-router-dom";
+import { Link } from 'react-router-dom'
 import imgErreur from '../media/img/erreur_page.jpg'
 import '../App.css';
 
 export default function ErrorPage() {
-    const error = useRouteError()
-    console.log(error)
-    console.log(error.error.message)
+
     return (
 
 
         <div class="erreur_page">
             <h1>Page notFound</h1>
-            <img src={imgErreur}/>
-            <p>Une impasse se dresse devant vous !</p>
-            
+            <img src={imgErreur} />
+            <div className="link2">
+                <p>Une impasse se dresse devant vous !</p>
+                <button><Link to={'/'}> Cliquez ici! </Link></button>
+            </div>
+
         </div>
     )
 }
