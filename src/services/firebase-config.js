fddece63e9b3fe667150dc99c654cabc51ed5b5e
ourabah <ourabah.lou@hotmail.com>
2023-09-18 14:50:03 +0200

    Update firebase-config.js

--- src/services/firebase-config.js
+++ src/services/firebase-config.js
@@ -1,23 +1,10 @@
-
-import { initializeApp }from "firebase/app";
+// Import des modules Firebase nécessaires
+import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "@firebase/firestore";
-import { getStorage } from "firebase/storage"
+import { getStorage } from "firebase/storage";
 
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-/*const firebaseConfig = {
-  apiKey: "AIzaSyAaZ12OTyXEq-qMu--38fc3-kYXUGmWrn0",
-  authDomain: "test-3a72e.firebaseapp.com",
-  projectId: "test-3a72e",
-  storageBucket: "test-3a72e.appspot.com",
-  messagingSenderId: "733939924500",
-  appId: "1:733939924500:web:f774433164f00d0b3db101",
-  
-};*/
+// Configuration Firebase récupérée à partir des variables d'environnement
 const firebaseConfig = {
   apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
   authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
@@ -25,11 +12,14 @@ const firebaseConfig = {
   storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
   messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
   appId: process.env.REACT_APP_FIREBASE_APP_ID,
-  measurementId: "G-Q78H751KKE"
+  measurementId: "G-F79K7F34HQ"
 };
 
-// Initialize Firebase
+// Initialise Firebase avec la configuration
 const app = initializeApp(firebaseConfig);
-export const db = getFirestore(app);
-export const auth = getAuth(app);
-export const storage = getStorage(app);
+
+// Exporte les instances des services Firebase pour les utiliser ailleurs dans l'application
+export const storage = getStorage(app); // Service Firebase Storage
+export const auth = getAuth(app);         // Service Firebase Auth (authentification)
+export const db = getFirestore(app);      // Service Firebase Firestore (base de données)
+
