7b55771bc83533d6a4da1da37f34dc4ae94edf7a
ourabah <ourabah.lou@hotmail.com>
2023-09-18 14:48:45 +0200

    Update Inscription.js

--- src/Routes/Inscription.js
+++ src/Routes/Inscription.js
@@ -1,17 +1,18 @@
 
 import Header from '../components/pages/Inscription/components/Header';
 import Main from '../components/pages/Inscription/components/Main';
-import Footer from '../components/pages/Inscription/components/Footer'
+import Footer from '../components/Commun/Footer'
 import React from 'react';
+
 
 export default function Inscription() {
     return (
         <>
-            
-                <Header />
-                <Main />
-                <Footer />
-           
+
+            <Header />
+            <Main />
+            <Footer />
+
         </>
 
     );
