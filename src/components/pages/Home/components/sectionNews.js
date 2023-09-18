b888ed00e06f2e290de3e55a9bfdf2a223d3050f
ourabah <ourabah.lou@hotmail.com>
2023-09-18 14:49:08 +0200

    Create sectionNews.js

---
+++ src/components/pages/Home/components/sectionNews.js
@@ -0,0 +1,24 @@
+import React  from 'react';
+import '../../../../App.css';
+import '../../../../appResponsive.css';
+
+
+
+function SectionNews() {
+   
+
+    return (
+        <section className="news">
+
+        <div className="content_news">
+            <h2>Newsletter</h2>
+            <p>Inscrivez-vous à notre newsletter pour rester informé(e) et connecté(e) avec Connectify !</p>
+            <input type="text" placeholder='Entrez votre email...' />
+            <button   type="submit">Valider</button>
+        </div>
+        
+                </section>
+    );
+}
+
+export default SectionNews;
